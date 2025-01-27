1deca10de0b8300010e5032aebabb790890a2312
Diogo Silva <56855879+DiogoJackson@users.noreply.github.com>
2025-01-26 23:50:11 -0300

    uploud graphics

--- R/glmm-binomial-tox.R
+++ R/glmm-binomial-tox.R
@@ -1,32 +1,41 @@
 # Packages
-library(ggplot2)
-library(dplyr)
+library(tidyverse)
 library(lmerTest)
 library(lme4)
 library(fitdistrplus)
-library(tidyverse)
 library(MuMIn)
 library(glmmTMB)
 library(DHARMa)
-library (gam)
-library (MASS)
-library (car)
+library(gam)
+library(MASS)
+library(car)
+library(inspecdf)
+library(ggpubr)
+library(ggdist)
 
 #Import data
 
-CGB <- read.csv("data/Processed/CGB_col.csv")
+CGB <- read.csv("data/Processed/CGB_col.csv") %>% 
+  select(-Collector,
+         -Notes,
+         -Date)
 
-CGB <- CGB[!is.na(CGB$"Climate"), ]
+any(duplicated(CGB))  # There is any duplicated rows?
+any(is.na(CGB))
+na_check <- inspectdf::inspect_na(CGB) # Percentage of NA in each columns
+na_check
+
+#CGB <- CGB[!is.na(CGB$"Climate"), ]
 CGB$live <- CGB$Ndaphnia - CGB$X240m #creates a column with n of Daphnia alive in the end of trial
-
 
 CGB$Site <- as.factor(CGB$Site) 
 CGB$Climate <- as.factor(CGB$Climate) 
 CGB$Sex <- as.factor(CGB$Sex) 
 
-CGB_subset <- na.omit(CGB[, c("X240m", "live", "Climate", "Sex", "Site", "SS", "death.p", "Lum_Mean.hd", "Lum_StdDev.hd", "Sat_Mean.hd", "Sat_StdDev.hd", 
-                              "Lum_Mean.fd", "Lum_StdDev.fd", "Sat_Mean.fd", "Sat_StdDev.fd", 
-                              "Lum_Mean.hv", "Lum_StdDev.hv", "Sat_Mean.hv", "Sat_StdDev.hv", 
+
+CGB_subset <- na.omit(CGB[, c("X240m", "live", "Climate", "Sex", "Site", "SS", "death.p", "Lum_Mean.hd", "Lum_StdDev.hd", "Sat_Mean.hd", "Sat_StdDev.hd",
+                              "Lum_Mean.fd", "Lum_StdDev.fd", "Sat_Mean.fd", "Sat_StdDev.fd",
+                              "Lum_Mean.hv", "Lum_StdDev.hv", "Sat_Mean.hv", "Sat_StdDev.hv",
                               "Lum_Mean.fv", "Lum_StdDev.fv", "Sat_Mean.fv", "Sat_StdDev.fv")])
 
 
@@ -36,19 +45,20 @@ lm_model <- lm(death.p ~ Climate + Sex +
                  Lum_Mean.fd +  Sat_Mean.fd +  
                  Lum_Mean.fv +  Sat_Mean.fv, data = CGB_subset)
 alias(lm_model) #removed variables that did not make too much sense, and site because of alias
-vif(lm_model) #all values below 3
+car::vif(lm_model) #all values below 3
 
 # Fit the model
 
 #### Including site does not improve the model (to few variation per site)
 
-m<- glm(cbind(X240m, live) ~ Climate + Sex + Lum_Mean.fd +  Sat_Mean.fd +  
+m <- glm(cbind(X240m, live) ~ Climate + Sex + Lum_Mean.fd +  Sat_Mean.fd +  
             Lum_Mean.fv +  Sat_Mean.fv, 
                   data = CGB_subset, 
                   family = binomial)
 
 summary(m)
 
+#Final model
 m_simple<- glm(cbind(X240m, live) ~ Climate + Sex, 
                data = CGB_subset, 
                family = binomial)
@@ -59,12 +69,22 @@ summary(m_simple)
 anova(m, m_simple)
 
 #### Plot data
-
-ggplot(CGB_subset, aes(x = Climate, y = death.p, fill=Sex)) +
-  geom_boxplot(position = "dodge") +
+p1 <- ggplot(CGB_subset, aes(x = Climate, y = death.p, fill = Sex)) +
+  geom_boxplot(alpha = 0.6) +
+  geom_point(aes(y = death.p, color = Sex), position = position_jitterdodge(), alpha = 0.7) +
   ylab("Death proportion")+
+  scale_fill_manual(values=c("#003459","#007ea7"))+
+  scale_color_manual(values=c("#003459","#007ea7"))+
   theme_classic(base_size = 16)
+p1
 
+p2 <- ggplot(CGB_subset, aes(x = Climate, y = death.p, fill = Sex)) +
+  geom_violin(color = "white",alpha = 0.3, width = 1, position = position_dodge(0.75)) +
+  geom_boxplot(aes(fill = Sex), width = 0.2, position = position_dodge(0.75), outlier.shape = NA, alpha = 0.5) +
+  scale_fill_manual(values=c("#003459","#007ea7"))+
+  ylab("Death proportion") +
+  theme_classic(base_size = 16)
+p2
 
 #################
 
